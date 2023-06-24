22586cab9ebd2d0a845d7fd34c0dafbe42d8c41a
Nate <natespring92@gmail.com>
2023-06-24 10:18:23 -0600

    pushin naked files

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    const data = req.body;
+    res.json(data);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
